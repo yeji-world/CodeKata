d467b089193fbb10cf8d5c3fae9f5d96e2a3d33d
yeji-world <lee24pm@gmail.com>
2024-11-04 10:29:36 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 2.20 ms, Memory: 77.3 MB -BaekjoonHub

--- Java/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ Java/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,30 +1,22 @@
+import java.util.*;
+
 class Solution {
     public long solution(long n) {
-        String num = Long.toString(n);
-
-        int digit[] = new int[num.length()];
-        int count = 0;
-        while (n>0) {
-            digit[count] = (int)(n % 10);
-            n /= 10;
-            count++;
-        }    
-
-        for (int i=0; i<digit.length-1; i++) {
-            for(int j=i+1; j<digit.length; j++){
-               if(digit[i]<digit[j]) {
-                    int temp = digit[i];
-                    digit[i] = digit[j];
-                    digit[j] = temp;
-               }
-            }        
-        } 
+       long answer = 0;
         
-        long answer = 0;
-        for (int i=0; i<digit.length; i++) {
-            answer = answer * 10 + digit[i];
-        }
-        return answer;
+        String num = String.valueOf(n);
+        String digit[] = num.split("");
 
+        Arrays.sort(digit);
+        String str = "";
+
+        for(int i = digit.length-1; i >= 0; i--){
+             str+= digit[i];
+        }
+
+        answer = Long.parseLong(str);      
+        
+
+        return answer;
     }
 }
