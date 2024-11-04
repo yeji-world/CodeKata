75bf166a6807c2c9bdc333e10c03b6ad1842031d
yeji-world <lee24pm@gmail.com>
2024-11-04 10:03:07 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 72.7 MB -BaekjoonHub

--- Java/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ Java/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,11 +1,9 @@
-import java.lang.*;
-
 class Solution {
     public long solution(long n) {
         long answer = 0;
         double x = Math.sqrt(n);
         
-        if (x%1==0) {
+        if (x%1 == 0) {
             answer = (long)((x+1)*(x+1));
         } else {
             answer = -1;
