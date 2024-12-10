19b33270e60350eedf24d4ba3d9ef1ef7cd24e76
yeji-world <lee24pm@gmail.com>
2024-12-10 11:47:40 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- Java/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ Java/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -16,40 +16,27 @@ class Solution {
                     highRanking ++;
                 }
         } 
-        switch(highRanking) {
-                case 6:
-                answer[0] = 1;
-                break;
-                case 5:
-                answer[0] = 2;
-                break;
-                case 4:
-                answer[0] = 3;
-                break;
-                case 3:
-                answer[0] = 4;
-                break;
-                case 2:
-                answer[0] = 5;
-                break;
-                default:
-                answer[0] = 6;
-                break;
-        }
-                switch(lowRanking) {
-                case 6: answer[1] = 1;
-                break;
-                case 5: answer[1] = 2;
-                break;
-                case 4: answer[1] = 3;
-                break;
-                case 3: answer[1] = 4;
-                break;
-                case 2: answer[1] = 5;
-                break;
-                default: answer[1] = 6;
-                break;
-        }
+        
+        answer[0] = rankLotto(highRanking);
+        answer[1] = rankLotto(lowRanking);
+        
         return answer;
     }
+        
+        public int rankLotto(int ranking) {            
+                switch(ranking) {
+                case 6:
+                return 1;
+                case 5:
+                return 2;
+                case 4:
+                return 3;
+                case 3:
+                return 4;
+                case 2:
+                return 5;
+                default:
+                return 6;                 
+                }
+        }        
 }
