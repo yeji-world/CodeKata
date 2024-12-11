8f09208842de3e701715a82dbc706b81436128e5
yeji-world <lee24pm@gmail.com>
2024-12-11 13:50:07 +0900

    [level 1] Title: 체육복, Time: 0.95 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        int answer = n-lost.length;
+
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i]==reserve[j]) {
+                    answer++;
+                    lost[i]=-1;
+                    reserve[j]=-1;
+                    break;
+                }
+            }
+        }
+        
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(Math.abs(lost[i]-reserve[j])==1){
+                    answer++;
+                    reserve[j]=-1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
