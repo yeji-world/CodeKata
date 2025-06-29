12bf48c9fac0b623a558204c29b151952e0b6736
yeji-world <lee24pm@gmail.com>
2025-06-29 14:19:31 +0900

    [Gold V] Title: 동전, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    amount = int(input())
+
+    dp = [0] * (amount + 1)
+    dp[0] = 1
+
+    for coin in coins:
+        for i in range(coin, amount + 1):
+            dp[i] += dp[i - coin]
+
+    print(dp[amount])
